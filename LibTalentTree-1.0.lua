b3240b89d15fef96e3f39f7ccbe3707cf28439b2
Mark W <numedain@gmail.com>
2023-12-03 13:26:41 +0100

    minor cleanup in annotations

--- LibTalentTree-1.0.lua
+++ LibTalentTree-1.0.lua
@@ -61,20 +61,20 @@ local BASE_PAN_OFFSET_Y = -30;
 ---@field targetNode number # TraitNodeID
 
 ---@class libNodeInfo
----@field ID: number # TraitNodeID
----@field posX: number
----@field posY: number
----@field type: nodeType # see Enum.TraitNodeType
----@field maxRanks: number
----@field flags: nodeFlags # see Enum.TraitNodeFlag
----@field groupIDs: number[]
----@field visibleEdges: visibleEdge[] # The order does not always match C_Traits
----@field conditionIDs: number[]
----@field entryIDs: number[] # TraitEntryID - generally, choice nodes will have 2, otherwise there's just 1
----@field specInfo: table<number, number[]> # specId: conditionType[] Deprecated, will be removed in 10.1.0; see Enum.TraitConditionType
----@field visibleForSpecs: table<number, boolean> # specId: true/false, true if a node is visible for a spec; added in 10.1.0
----@field grantedForSpecs: table<number, boolean> # specId: true/false, true if a node is granted for free, for a spec; added in 10.1.0
----@field isClassNode: boolean
+---@field ID number # TraitNodeID
+---@field posX number
+---@field posY number
+---@field type nodeType # see Enum.TraitNodeType
+---@field maxRanks number
+---@field flags nodeFlags # see Enum.TraitNodeFlag
+---@field groupIDs number[]
+---@field visibleEdges visibleEdge[] # The order does not always match C_Traits
+---@field conditionIDs number[]
+---@field entryIDs number[] # TraitEntryID - generally, choice nodes will have 2, otherwise there's just 1
+---@field specInfo table<number, number[]> # specId: conditionType[] Deprecated, will be removed in 10.1.0; see Enum.TraitConditionType
+---@field visibleForSpecs table<number, boolean> # specId: true/false, true if a node is visible for a spec; added in 10.1.0
+---@field grantedForSpecs table<number, boolean> # specId: true/false, true if a node is granted for free, for a spec; added in 10.1.0
+---@field isClassNode boolean
 
 ---@class entryInfo
 ---@field definitionID number # TraitDefinitionID
